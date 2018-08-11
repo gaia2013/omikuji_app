500e58919b0d1260009da1d0b75327550c58bbd3
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2018-08-11 10:10:15 +0900

    Initial Commit

---
+++ OmikujiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OmikujiApp
+//
+//  Created by 山口仁志 on 2018/08/11.
+//  Copyright © 2018年 OmikujiApp.hitoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
